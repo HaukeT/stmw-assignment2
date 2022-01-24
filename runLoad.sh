c3e3d07f0a979874cd8fdf5e81a673c65201ca2c
Hauke <hauke2@uni-bremen.de>
2022-01-24 14:15:58 +0100

    shell script correction

--- runLoad.sh
+++ runLoad.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+# Remove all temporary files
+rm *.class
+
 mysql -u ad < dropSpatialIndex.sql
 
 mysql -u ad < createSpatialIndex.sql
 
 sh ./run-Indexer.sh
 javac -cp /usr/share/java/mysql-connector-java-5.1.28.jar:/usr/share/java/lucene-core-5.4.0.jar:/usr/share/java/lucene-analyzers-common-5.4.0.jar:/usr/share/java/lucene-queryparser-5.4.0.jar:/usr/share/java/lucene-queries-5.4.0.jar:. Searcher.java
-
-# Remove all temporary files
-rm *.class
-rm *.csv
