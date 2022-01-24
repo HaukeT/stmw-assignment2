708973bdce8aea1431f58db55f3b64da0b7dbaf8
Hauke <hauke2@uni-bremen.de>
2022-01-24 15:47:58 +0100

    scheiss alte sql version4.1

--- runLoad.sh
+++ runLoad.sh
@@ -7,5 +7,6 @@ mysql ad < dropSpatialIndex.sql
 
 mysql ad < createSpatialIndex.sql
 
+export CLASSPATH="/usr/share/java/mysql-connector-java-5.1.28.jar:/usr/share/java/lucene-core-5.4.0.jar:/usr/share/java/lucene-analyzers-common-5.4.0.jar:/usr/share/java/lucene-queryparser-5.4.0.jar:/usr/share/java/lucene-queries-5.4.0.jar:."
 sh ./run-Indexer.sh
-javac -cp /usr/share/java/mysql-connector-java-5.1.28.jar:/usr/share/java/lucene-core-5.4.0.jar:/usr/share/java/lucene-analyzers-common-5.4.0.jar:/usr/share/java/lucene-queryparser-5.4.0.jar:/usr/share/java/lucene-queries-5.4.0.jar:. Searcher.java
+javac Searcher.java
